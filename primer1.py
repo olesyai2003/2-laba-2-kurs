3c3c9d4336efe697ca181dbe95bc6464b296a353
Олеся Иващенко <105241919+olesyai2003@users.noreply.github.com>
2023-01-27 13:28:24 +0300

    Пример 1

---
+++ primer1.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def decorator_function(func):
+    def wrapper():
+        print('Функция-обёртка!')
+        print('Оборачиваемая функция: {}'.format(func))
+        print('Выполняем обёрнутую функцию...')
+        func()
+        print('Выходим из обёртки')
+
+    return wrapper
+
+
+@decorator_function
+def hello_world():
+    print('Hello world!')
+
+
+hello_world()
